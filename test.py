53ab6b9bc9a5a183edd78225f6f3ac2952b5d2db
Andy <amueller@ais.uni-bonn.de>
2011-08-18 14:45:57 +0200

    removed old stuff..

--- test.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-from pyquickshift import quickshift
-import numpy as np
-im = plt.imread("flowers2.pnm")
-image, segments = quickshift(im.astype(np.ubyte),10,6,0)
-plt.imshow(image)
-plt.figure()
-plt.imshow(segments)
-plt.show()
