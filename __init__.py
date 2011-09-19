bbec77fa448089b374120f8a4458d9dc87b1f3b0
Andy <amueller@ais.uni-bonn.de>
2011-09-19 12:07:03 +0200

    changed test image to lena, flipping less weired...

--- __init__.py
+++ __init__.py
@@ -2,16 +2,19 @@ import pyquickshift
 
 def quickshift(img, tau, sigma, device=-1):
     image,segments = pyquickshift.quickshift(img, tau, sigma, device)
-    return image[::-1,:], segments
+    return image, segments[::-1,:]
 
 def demo():
     import matplotlib.pyplot as plt
     import numpy as np
     import os
-    im = plt.imread(os.path.join(os.path.dirname(__file__),"flowers2.pnm"))
-    image, segments = quickshift(im.astype(np.ubyte),10,6,0)
+    im = plt.imread(os.path.join(os.path.dirname(__file__),"lena.png"))
+    image, segments = quickshift((im*255).astype(np.ubyte),10,6,-1)
+    plt.subplot(131)
+    plt.imshow(im)
+    plt.subplot(132)
     plt.imshow(image)
-    plt.figure()
+    plt.subplot(133)
     plt.imshow(segments)
     plt.show()
 
